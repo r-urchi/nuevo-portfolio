7617d9789b7fcc63f511c242831877098caa9d88
Ramon Urchipia <ramonurchipia@gmail.com>
2021-03-06 11:26:39 -0300

    actualizo proyectos

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,10 +7,10 @@ const AboutMe = () => {
             <h2>SOBRE MÍ</h2>
             <span></span>
             <p>Vivo en Capital Federal, estoy comenzando algunos proyectos como desarrollador freelance. Mi principal objetivo es convertir el desarrollo como trabajo de tiempo completo. <br/>
-            Comencé a aprender sobre desarrollo como autodidacta en 2019. En 2020 me inscribí en la carrera de Desarrollador Front End en Coderhouse, donde tuve la oportunidad de reforzar conocimientos en Desarrollo Web utilizando HTML, Css, Sass, Git, Github. <br/> 
+            Comencé a aprender sobre desarrollo como autodidacta en 2019. <br/> En 2020 me inscribí en la carrera de Desarrollador Front End en Coderhouse, donde tuve la oportunidad de reforzar conocimientos en Desarrollo Web utilizando HTML, Css, Sass, Git, Github. <br/> 
             Luego en el curso de JavaScript, y finalmente con el de React. Todos los proyectos que fui desarrollando en estos cursos se pueden encontrar en mi portfolio.<br/>
             Actualmente estoy realizando el curso de Diseño UX / UI, utilizando herramientas de diseño, principalmente Figma. <br/>
-            En poco tiempo voy a estar comenzando con la parte de Back-End, donde voy a estar aprendiendo sobre Node.js. </p>
+            En poco tiempo voy a estar comenzando con Back-End, donde voy a estar aprendiendo sobre Node.js. </p>
         </div>
     )
 }
